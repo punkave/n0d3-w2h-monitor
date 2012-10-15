b0d095b996a3c593ef8129b366f3e7499ff05baf
Gabriel Farrell <g@grrawr.com>
2012-10-15 11:23:37 -0400

    No cheeky messages. Now with timestamps.

--- monitor.js
+++ monitor.js
@@ -1,36 +1,21 @@
 var request = require('request');
 
 var sites = {};
-var happyNews = [
-  'Oh joyful joyousness, everyone!',
-  'Everyone, it\'s a Xmas miracle!',
-  'Good job, everyone!'
-];
-var sadNews = [
-  'Uh oh spaggetios, everyone.',
-  'Everyone, I have bad news, and I have bad news.',
-  'Everyone, I am just the messenger.'
-];
 
 module.exports = function monitor (room, log) {
   setInterval(function () {
+    var timestamp = new Date();
     request({
       url: 'http://monitor.waytohealth.org',
       json: true
     }, function (err, res, body) {
       body.forEach(function (site) {
-        var news;
         if (sites[site.slug] === undefined) {
           sites[site.slug] = true;
           return;
         }
         if (sites[site.slug] != site.ok) {
-          if (site.ok) {
-            news = randomSelect(happyNews);
-          } else {
-            news = randomSelect(sadNews);
-          }
-          room.speak('*** ' + news + ' ' + site.name + ' (' + site.url + ') is ' + (site.ok ? 'UP' : 'DOWN') + ' ***')
+          room.speak('everyone: ' + timestamp + (site.ok ? ' UP   ' : ' DOWN ') + site.url);
           sites[site.slug] = site.ok;
         }
       })
